a7b2297c36f7f9f8ff6a56804aae961470d090f5
nikoelvambuena95 <38633487+nikoelvambuena95@users.noreply.github.com>
2021-05-11 15:47:45 -0700

    append data to html table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,15 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody")
+
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoReport).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value)
+    })
+});
